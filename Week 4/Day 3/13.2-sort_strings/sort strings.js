4a5c55ead5183deb61127a701a8db55b734bae60
IlyaMeli <ilyameli@gmail.com>
2021-11-16 11:21:20 +0200

    sort

--- Week 4/Day 3/13.2-sort_strings/sort strings.js
+++ Week 4/Day 3/13.2-sort_strings/sort strings.js
@@ -1,38 +1,38 @@
-// const foods = ["falafel", "sabich", "hummus", "pizza with extra pineapple"];
+const foods = ["falafel", "sabich", "hummus", "pizza with extra pineapple"];
 
-// console.log(foods.sort());
-// console.log(foods.sort().reverse());
+console.log(foods.sort());
+console.log(foods.sort().reverse());
 
-// const foodsWithUpperCase = [
-//   "falafel",
-//   "Sabich",
-//   "hummus",
-//   "pizza with extra pineapple",
-// ];
-// //ascending
-// console.log(
-//   foodsWithUpperCase.sort((a, b) => {
-//     if (a.toLowerCase() > b.toLowerCase()) {
-//       return -1;
-//     }
-//     if (a.toLowerCase() < b.toLowerCase()) {
-//       return 1;
-//     }
-//     return 0;
-//   })
-// );
-// //descending
-// console.log(
-//   foodsWithUpperCase.sort((a, b) => {
-//     if (a.toLowerCase() < b.toLowerCase()) {
-//       return -1;
-//     }
-//     if (a.toLowerCase() > b.toLowerCase()) {
-//       return 1;
-//     }
-//     return 0;
-//   })
-// );
+const foodsWithUpperCase = [
+  "falafel",
+  "Sabich",
+  "hummus",
+  "pizza with extra pineapple",
+];
+//ascending
+console.log(
+  foodsWithUpperCase.sort((a, b) => {
+    if (a.toLowerCase() > b.toLowerCase()) {
+      return -1;
+    }
+    if (a.toLowerCase() < b.toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  })
+);
+//descending
+console.log(
+  foodsWithUpperCase.sort((a, b) => {
+    if (a.toLowerCase() < b.toLowerCase()) {
+      return -1;
+    }
+    if (a.toLowerCase() > b.toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  })
+);
 
 const words = ["apple", "supercalifragilisticexpialidocious", "hi", "zoo"];
 
