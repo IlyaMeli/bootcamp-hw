06b7de8f30cf008de9496a32834712e40fb67974
IlyaMeli <ilyameli93@gmail.com>
2021-11-21 20:05:04 +0200

    binary

--- Week 5/Day 1/1. practice recursion/practice-recursion.js
+++ Week 5/Day 1/1. practice recursion/practice-recursion.js
@@ -23,7 +23,6 @@ function reverseString4(str) {
 }
 
 //ex 4
-
 function compute(n, p) {
   if (p === 1) return n;
   console.log(n + " and " + p);
@@ -31,3 +30,36 @@ function compute(n, p) {
 }
 
 console.log(compute(2, 3));
+
+//5
+function binarySearch(arr, goal) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (true) {
+    mid = Math.floor((start + end) / 2); // creating floored middle point
+    if (goal === arr[mid]) {
+      // exit statment.
+      return mid;
+    }
+    if (goal > arr[mid]) {
+      start = mid + 1;
+    } else if (goal < arr[mid]) {
+      end = mid - 1;
+    }
+  }
+}
+
+const arr = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91];
+
+console.log(binarySearch(arr, 23));
+
+//recursion binary
+function binarySearchRecursive(arr, goal, start = 0, end = arr.length - 1) {
+  mid = Math.floor((start + end) / 2);
+  if (goal === arr[mid]) {
+    return mid;
+  }
+  if (goal > arr[mid]) return binarySearchRecursive(arr, goal, mid + 1, end);
+  else if (goal < arr[mid])
+    return binarySearchRecursive(arr, goal, start, mid - 1);
+}
