17ae5f0097d8e4e6323af7727e33c36633ce2eab
IlyaMeli <ilyameli93@gmail.com>
2021-11-24 11:34:32 +0200

    Day 4 DOM

--- Week 5/Day 2 - 3/practice data structures/practice.js
+++ Week 5/Day 2 - 3/practice data structures/practice.js
@@ -1,3 +1,26 @@
-for (let i = 100; i > -5; i = i - 5) {
-  console.log(i);
-}
+//1
+const printToN = (n) => {
+  for (var i = 0; i < n + 1; i++) {
+    console.log(i);
+  }
+};
+//2
+const countX = (n, t) => {
+  for (let i = 0; i < n; i++) {
+    if (i % t == 0) {
+      console.log(i);
+    }
+  }
+};
+//3
+const countEven = (n) => {
+  let counter = 0;
+  let str = n.toString();
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] % 2 === 0) {
+      counter++;
+    }
+  }
+  return counter;
+};
+console.log(countEven(28000000000031867848641521981));
