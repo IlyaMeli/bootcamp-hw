2d3c7a598f2800ea1ec740bd2cb66ebc2606e00e
IlyaMeli <ilyameli93@gmail.com>
2021-11-28 14:41:46 +0200

    19.3

---
+++ Week 6/Day -1/19.3 copy muliple inputs/index.js
@@ -0,0 +1,22 @@
+let input = document.querySelector(".input");
+let inputs = document.querySelectorAll(".input");
+let submit = document.getElementById("submit");
+
+for (let i = 0; i < inputs.length; i++) {
+  inputs[0].addEventListener("paste", (e) => {
+    if (e.clipboardData.getData("text")[i] !== undefined) {
+      inputs[i].value = e.clipboardData.getData("text")[i];
+    }
+    if (inputs[inputs.length - 1].value) {
+      submit.click();
+    }
+  });
+  inputs[i].addEventListener("input", () => {
+    if (inputs[i].value) {
+      inputs[i + 1].focus();
+    }
+    if (inputs[i].value.length > 1) {
+      inputs[i].value = inputs[i].value.substr(0, 1);
+    }
+  });
+}
