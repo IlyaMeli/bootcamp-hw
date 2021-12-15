2bf5eee1392648eba2dda8f836f214d4ba2d8957
IlyaMeli <ilyameli93@gmail.com>
2021-12-15 20:35:34 +0200

    11.2 DONE!

--- Week 8/Day - 4/11.2 checkbox/src/Input.js
+++ Week 8/Day - 4/11.2 checkbox/src/Input.js
@@ -1,7 +1,17 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 export default class Input extends Component {
-  render() {
-    return <div></div>;
-  }
+    render() {
+        return (
+            <>
+            <input
+              onChange={this.props.handleChange}
+              type={this.props.inputType}
+              name={this.props.inputName}
+              checked={this.props.isChecked}
+            ></input>
+            <label>{this.props.inputName}</label>
+          </>
+        )
+    }
 }
